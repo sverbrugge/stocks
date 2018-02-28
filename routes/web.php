c635e5c0b31aed713a8963f48845db7277965953
Sander Verbrugge <32176563+sverbrugge@users.noreply.github.com>
2018-02-28 14:57:38 +0100

    User scaffold

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/home', 'HomeController@index')->name('home');
