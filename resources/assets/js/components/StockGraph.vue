93ff2866ef9562d457b31ce49c4bd3a792d1c346
Sander Verbrugge <32176563+sverbrugge@users.noreply.github.com>
2019-04-20 15:17:51 +0200

    Horizontal plot lines added for bought shares

--- resources/assets/js/components/StockGraph.vue
+++ resources/assets/js/components/StockGraph.vue
@@ -4,22 +4,106 @@
 
 <script>
     import highstock from 'highcharts/highstock';
+    require('highcharts/modules/debugger')(highstock);
     import axios from 'axios';
     export default {
-        props: ['id'],
+        props: [
+            'id',
+            'name',
+            'lines'
+        ],
 
         mounted() {
             axios.get('/api/graph/stock/' + this.id).then(response => {
-                highstock.stockChart({
+                let date = new Date();
+                let options = {
                     chart: {
                         renderTo: this.$el
                     },
+                    rangeSelector: {
+                        selected: (date.getDay() > 0 && date.getDay() < 6) ? 0 : 1,
+
+                        buttons: [
+            				{
+            					type: 'day',
+            					count: 1,
+            					text: '1d'
+            				},
+            				{
+            					type: 'week',
+            					count: 1,
+            					text: '1w'
+            				},
+            				{
+            					type: 'week',
+            					count: 2,
+            					text: '2w'
+            				},
+                            {
+            					type: 'month',
+            					count: 1,
+            					text: '1m'
+            				},
+                            {
+            					type: 'month',
+            					count: 3,
+            					text: '3m'
+            				},
+            				{
+            					type: 'month',
+            					count: 6,
+            					text: '6m'
+            				},
+            				{
+            					type: 'ytd',
+            					text: 'YTD'
+            				},
+            				{
+            					type: 'year',
+            					count: 1,
+            					text: '1y'
+            				},
+            				{
+            					type: 'all',
+            					text: 'All'
+            				}
+            			]
+                    },
+                    yAxis: {
+                        plotLines: []
+                    },
                     series: [
                         {
-                            data: response.data
+                            name: this.name,
+                            data: response.data,
+                            tooltip: {
+                                valueDecimals: 4
+                            }
                         }
                     ]
-                });
+                };
+
+                if (this.lines) {
+                    try {
+                        let lines = JSON.parse(this.lines);
+                        lines.forEach(line => {
+                            options.yAxis.plotLines.push({
+                                color: '#ff0000',
+                                dashStyle: 'shortdash',
+                                zIndex: 10,
+                                width: 2,
+                                value: parseFloat(line),
+                                label: {
+                                    text: line
+                                }
+                            });
+                        });
+                    } catch (e) {
+                        // do nothing
+                    }
+                }
+                console.log(options.yAxis.plotLines);
+                highstock.stockChart(options);
             });
         }
     }
--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-12">
             <h1>{{ $stock->name }} <small class="text-muted">{{ $stock->ticker }}</small></h1>
 
-            @foreach ($stock->activeShares as $share)
+            @foreach ($stock->activeShares->unique('price') as $share)
                 <h2>
                     {{ $share->price }}
                     <small class="text-{{ $share->colorClass }}" title="{{ $stock->currentQuote->quoted_at }} ({{ $stock->currentQuote->quoted_at->diffForHumans() }})">
@@ -16,7 +16,7 @@
                 </h2>
             @endforeach
 
-            <stock-graph id="{{ $stock->id }}"></stock-graph>
+            <stock-graph id="{{ $stock->id }}" name="{{ $stock->ticker }}" lines="{{ $stock->activeShares->unique('price')->pluck('price') }}"></stock-graph>
         </div>
     </div>
 </div>
