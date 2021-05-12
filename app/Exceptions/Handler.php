c3d2022e60e7b3fbdaa499a658b8cb66c05d3a7f
Sander Verbrugge <32176563+sandcore-dev@users.noreply.github.com>
2021-05-12 22:20:57 +0200

    Fixed return type of exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
